fd06547b206ff60cf45d2744e27cfbbe8d0c3fd6
IaraTassi <iaratassi@hotmail.com>
2025-10-31 19:46:42 -0300

    refactor(services): aplicar validações usando métodos de busca dos repositories

--- src/services/follow.service.ts
+++ src/services/follow.service.ts
@@ -9,6 +9,16 @@ export class FollowService {
     if (!valor?.trim()) throw new Error(mensagem);
   }
 
+  async buscarFollow(
+    followerId: string,
+    followingId: string
+  ): Promise<Follow | null> {
+    this.validarCampo(followerId, "O ID do seguidor é obrigatório.");
+    this.validarCampo(followingId, "O ID do usuário seguido é obrigatório.");
+
+    return this.followRepository.buscarFollow(followerId, followingId);
+  }
+
   async seguirUsuario(dto: FollowDto, followerId: string): Promise<Follow> {
     this.validarCampo(followerId, "O ID do seguidor é obrigatório.");
     this.validarCampo(
@@ -19,8 +29,14 @@ export class FollowService {
     if (followerId === dto.followingId)
       throw new Error("Um usuário não pode seguir a si mesmo.");
 
-    const follow = await this.followRepository.seguirUsuario(dto, followerId);
+    const relacionamentoExistente = await this.followRepository.buscarFollow(
+      followerId,
+      dto.followingId
+    );
+    if (relacionamentoExistente)
+      throw new Error("O usuário já está seguindo este perfil.");
 
+    const follow = await this.followRepository.seguirUsuario(dto, followerId);
     return follow;
   }
 
@@ -36,6 +52,13 @@ export class FollowService {
 
     if (followerId === followingId)
       throw new Error("Um usuário não pode deixar de seguir a si mesmo.");
+
+    const relacionamentoExistente = await this.followRepository.buscarFollow(
+      followerId,
+      followingId
+    );
+    if (!relacionamentoExistente)
+      throw new Error("O usuário não segue este perfil.");
 
     await this.followRepository.deixarDeSeguirUsuario(followerId, followingId);
   }
