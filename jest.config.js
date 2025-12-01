a84b277cf3bd8b605c5076f90d51e308f5197444
IaraTassi <iaratassi@hotmail.com>
2025-12-01 14:39:38 -0300

    docs: atualizar README e arquivos de configuração para deploy

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["./tests/setup.ts"],
   runInBand: true,
   maxWorkers: 1,
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
 };
