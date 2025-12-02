6e00f54dd344dfe0086037f04693b843c651c06b
IaraTassi <iaratassi@hotmail.com>
2025-12-02 07:42:25 -0300

    remove fallback para 3000

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from "./app";
 
-const PORT: number = Number(process.env.PORT) || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
