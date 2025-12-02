9cbb86db41fd2349f5cb14343702c4a4d013a100
IaraTassi <iaratassi@hotmail.com>
2025-12-02 11:59:36 -0300

    chore(segurança): adicionar verificações de ambiente temporárias para validar a configuração do ambiente de renderização

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+console.log("JWT_SECRET definida?", !!process.env.JWT_SECRET);
+console.log("DATABASE_URL definida?", !!process.env.DATABASE_URL);
+
 import app from "./app";
 
 const PORT = process.env.PORT;
