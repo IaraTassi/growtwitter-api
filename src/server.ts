e0d51d00f728e85c50fe7a26483f09a92089ac5f
IaraTassi <iaratassi@hotmail.com>
2025-12-02 12:04:18 -0300

    chore(segurança): remover verificações de ambiente temporárias após a validação

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-console.log("JWT_SECRET definida?", !!process.env.JWT_SECRET);
-console.log("DATABASE_URL definida?", !!process.env.DATABASE_URL);
-
 import app from "./app";
 
 const PORT = process.env.PORT;
